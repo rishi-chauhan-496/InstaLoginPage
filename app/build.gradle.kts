9d6586f491443653d64a90a1c5a064123e04ab87
chauhan-rishi-496 <chauhanrishi496@gmail.com>
2026-01-03 08:57:30 +0530

    Change to Forgotten Password page UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.benchmark.traceprocessor)
+    implementation(libs.androidx.compose.foundation)
     // Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
